3512c10832cb691bc21d5445c5401d1642fe4dcc
Jesper Sandström <jespersandstrom@gmail.com>
2017-11-02 16:25:03 +0100

    Remove diffs

--- diff.hs
+++
@@ -1,8 +0,0 @@
-main =
-  do inp <- fmap (map show . map solve . map (map read') . map words . lines) getContents
-     mapM_ putStrLn inp
-
-read' :: String -> Integer
-read' = read
-
-solve (a:b:[]) = abs (a - b)
